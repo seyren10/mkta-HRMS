05a8c1f678f642b5c4ba3d0b3546107e44800f78
seyren10 <87021985+seyren10@users.noreply.github.com>
2024-02-15 15:04:55 +0800

    Notification Fix
    Fixed:
    -different type of notification displays different types of data
    -violation and violation types logic on DeletePendingNotification Job wasnt right
    
    Added:
    -Manual and automatic rejection of pending violation

--- app/Jobs/DeletePendingViolations.php
+++ app/Jobs/DeletePendingViolations.php
@@ -6,10 +6,13 @@ use App\Http\Resources\PendingViolationResource;
 use App\Models\EmployeeViolation;
 use App\Models\Log;
 use App\Models\PendingViolation;
+use App\Models\Setting;
+use App\Notifications\ViolationReminder;
 use App\Notifications\ViolationSuccess;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
@@ -24,9 +27,10 @@ class DeletePendingViolations implements ShouldQueue
     /**
      * Create a new job instance.
      */
-    public function __construct(array $pendingViolations, $user)
+    public function __construct(Collection $pendingViolations, $user)
     {
         $this->pendingViolations = $pendingViolations;
+
         $this->user = $user;
     }
 
@@ -35,28 +39,29 @@ class DeletePendingViolations implements ShouldQueue
      */
     public function handle(): void
     {
-        $pendingViolations = PendingViolation::whereIn('employee_id', $this->pendingViolations)->get();
-        info('empty: ' . empty($pendingViolations) . ' count: ' . count($pendingViolations));
-        if (count($pendingViolations) === 0)  return;
+        if (!$this->pendingViolations->count())  return;
 
-
-
-        foreach ($pendingViolations as $data) {
-            EmployeeViolation::create([
-                'employee_id' => $data->employee_id,
-                'violation_id' => $data->violation_id
-            ]);
-        }
+        $automaticRejection = Setting::where("name", "automaticRejection")->first();
 
         Request::merge(['includeViolationType' => true]);
 
-        Log::create([
-            'type' => 'pendingViolation',
-            'data' => json_encode(PendingViolationResource::collection($pendingViolations))
-        ]);
+        if ($automaticRejection->value) {
+            foreach ($this->pendingViolations as $data) {
+                EmployeeViolation::create([
+                    'employee_id' => $data->employee_id,
+                    'violation_id' => $data->violation_id
+                ]);
+                $data->delete();
+            }
 
-        PendingViolation::whereIn('employee_id', $this->pendingViolations)->delete();
+            Log::create([
+                'type' => 'pendingViolation',
+                'data' => json_encode(PendingViolationResource::collection($this->pendingViolations))
+            ]);
 
-        $this->user->notify(new ViolationSuccess($pendingViolations));
+
+            $this->user->notify(new ViolationSuccess($this->pendingViolations));
+        } else
+            $this->user->notify(new ViolationReminder($this->pendingViolations));
     }
 }
